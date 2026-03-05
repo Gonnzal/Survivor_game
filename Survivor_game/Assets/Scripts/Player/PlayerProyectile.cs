2c5ee43b1b27e7eb8e027c6534f578571a779884
Ibai Gonzalez <ibai_gonzalez@euneiz.com>
2026-03-05 23:29:09 +0100

    Add: player ranged attack

---
+++ Survivor_game/Assets/Scripts/Player/PlayerProyectile.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Unity.VisualScripting;
+using UnityEngine.UIElements;
+
+public class PlayerProyectile : MonoBehaviour
+{
+    public float speed;
+    public Vector3 target;
+    public float damage;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Survivor_game/Assets/Scripts/Player/rangedAttack.cs
@@ -0,0 +1,78 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class rangedAttack : MonoBehaviour
+{
+    [SerializeField] GameObject enemy;
+    [SerializeField] float coolDown;
+    [SerializeField] float proyectileSpeed;
+    [SerializeField] GameObject proyectile;
+    [SerializeField] float damage;
+    float distanceToTarget;
+    float tempDistance;
+    bool canShoot = true;
+    Vector3 target;
+    GameObject[] proyectiles = new GameObject[] {};
+    int[] poolIndex = new int[] {};
+    [SerializeField] int proyectileCount;
+    [SerializeField] 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = 0; i < proyectileCount+1; i++)
+        {
+            GameObject temp = Instantiate(proyectile, transform.position, transform.rotation);
+            temp.GetComponent<PlayerProyectile>().speed = proyectileSpeed;
+            temp.GetComponent<PlayerProyectile>().damage = damage;
+            System.Array.Resize(ref proyectiles, proyectiles.Length + 1);
+            proyectiles[proyectiles.Length - 1] = temp;
+            temp.SetActive(false);
+            System.Array.Resize(ref poolIndex, poolIndex.Length +1);
+            poolIndex[poolIndex.Length-1] = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canShoot)
+        {
+            foreach (GameObject obj in )
+            {
+                MeasureDistance(obj);
+                if(tempDistance < distanceToTarget)
+                {
+                    SelectTarget(obj);
+                }
+            }
+
+            for (int i = 0; i<poolIndex.Length; i++)
+            {
+                if(poolIndex[i] == 0)
+                {
+                    proyectiles[i].SetActive(true);
+                    proyectiles[i].GetComponent<PlayerProyectile>().target = target;
+                }
+            }
+        }
+    }
+
+    void MeasureDistance(GameObject obj)
+    {
+        float distanceX = this.transform.position.x - obj.transform.position.x;
+        float distanceY = this.transform.position.y - obj.transform.position.y;
+        float distanceZ = this.transform.position.z - obj.transform.position.z;
+        tempDistance = Mathf.Sqrt(distanceX * distanceX + distanceY * distanceY + distanceZ * distanceZ);
+    }
+
+    void SelectTarget(GameObject obj)
+    {
+        target = obj.transform.position;
+    }
+
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(coolDown);
+    }
+}
