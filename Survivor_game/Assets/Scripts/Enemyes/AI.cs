0be99e09bd8d66793432145a8ec629cb4a684655
Ibai Gonzalez <ibai_gonzalez@euneiz.com>
2026-03-05 22:06:32 +0100

    Improoved: Enemy movement towards player Adjust: prefabs, to make preparing scenes easyer

--- Survivor_game/Assets/Scripts/Enemyes/AI.cs
+++ Survivor_game/Assets/Scripts/Enemyes/AI.cs
@@ -9,6 +9,7 @@ public class AI : MonoBehaviour
     [SerializeField] public float damage;
     [SerializeField] public float cooldown;
     [SerializeField] Transform player;
+    [SerializeField] float distanceDifference;
     public bool canAttack = true;
     public float distanceX;
     public float distanceY;
@@ -30,46 +31,7 @@ public class AI : MonoBehaviour
 
         if(distance > range)
         {
-            GetCloser();
-        }
-    }
-
-    void GetCloser()
-    {
-        float angle = Mathf.Atan2(distanceY, distanceX) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-        if(distanceX == 0)
-        {
-            transform.position = new Vector2(transform.position.x, transform.position.y);
-        }
-        else if(distanceX > -0.3f && distanceX < 0.3f)
-        {
-            transform.position = new Vector2(player.transform.position.x, transform.position.y);
-        }
-        else if(distanceX > 0.3f)
-        {
-            transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
-        }
-        else if (distanceX < -0.3f)
-        {
-            transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
-        }
-
-        if(distanceY == 0)
-        {
-            transform.position = new Vector2(transform.position.x, transform.position.y);
-        }
-        else if(distanceY > -0.3f && distanceY < 0.3f)
-        {
-            transform.position = new Vector2(transform.position.x, player.transform.position.y);
-        }
-        else if (distanceY > 0.3f)
-        {
-            transform.position = new Vector2(transform.position.x, transform.position.y + speed * Time.deltaTime);
-        }
-        else if (distanceY < -0.3f)
-        {
-            transform.position = new Vector2(transform.position.x, transform.position.y - speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
         }
     }
 }
