ae42b2923d2d8053418ae8881e1189823800ad1d
Ibai Gonzalez <ibai_gonzalez@euneiz.com>
2026-03-08 20:06:45 +0100

    canvas changing logic

--- Survivor_game/Assets/Scripts/New/Rounds.cs
+++ Survivor_game/Assets/Scripts/New/Rounds.cs
@@ -6,16 +6,25 @@ public class Rounds : MonoBehaviour
     [SerializeField] int roundCount;
     [SerializeField] TMP_Text timeCounter;
     [SerializeField] float roundDuration;
+    [SerializeField] CanvasGroup gameCanvas;
+    [SerializeField] CanvasGroup mainMenuCanvas;
+    [SerializeField] CanvasGroup creditsCanvas;
+    [SerializeField] CanvasGroup deathCanvas;
+    [SerializeField] CanvasGroup finishCanvas;
+    [SerializeField] CanvasGroup canvasBetweenRounds;
+    [SerializeField] EnemyAI enemyHealth;
     int currentRound;
     float roundProgress;
     int minuteCount;
     int second2Count;
     int second1Count;
+    public bool roundFinished;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currentRound = 0;
         timeCounter.text = minuteCount.ToString() + ":" + second1Count.ToString() + second2Count.ToString();
+        roundFinished = false;
     }
 
     // Update is called once per frame
@@ -47,15 +56,111 @@ public class Rounds : MonoBehaviour
 
     void FinishRound()
     {
-        /*
-        aquí hay que: 
-        1. hacer desaparecer a todos los enemigos
-        2. hacer que aparezca la tienda o menú entre rondas
-        */
+        roundFinished = true;
+        canvasBetweenRounds.interactable = true;
+        canvasBetweenRounds.alpha = 1;
+        gameCanvas.interactable = false;
+        gameCanvas.alpha = 0;
+        Time.timeScale = 0;
+        enemyHealth.ReciveDanio(9999);
+        //hacer desaparecer a todos los enemigos
     }
     
     void BossRound()
     {
         
+    }
+
+    public void StartRound()
+    {
+        Time.timeScale = 1;
+        currentRound++;
+        if(currentRound == roundCount)
+        {
+            BossRound();
+        }
+    }
+
+    public void MainMenu()
+    {
+        mainMenuCanvas.interactable = true;
+        mainMenuCanvas.alpha = 1;
+        canvasBetweenRounds.interactable = false;
+        canvasBetweenRounds.alpha = 0;
+        gameCanvas.interactable = false;
+        gameCanvas.alpha = 0;
+        finishCanvas.interactable = false;
+        finishCanvas.alpha = 0;
+        deathCanvas.interactable = false;
+        deathCanvas.alpha = 0;
+        creditsCanvas.interactable = false;
+        creditsCanvas.alpha = 0;
+    }
+
+    public void Credits()
+    {
+        mainMenuCanvas.interactable = false;
+        mainMenuCanvas.alpha = 0;
+        canvasBetweenRounds.interactable = false;
+        canvasBetweenRounds.alpha = 0;
+        gameCanvas.interactable = false;
+        gameCanvas.alpha = 0;
+        finishCanvas.interactable = false;
+        finishCanvas.alpha = 0;
+        deathCanvas.interactable = false;
+        deathCanvas.alpha = 0;
+        creditsCanvas.interactable = true;
+        creditsCanvas.alpha = 1;
+    }
+
+    public void Game()
+    {
+        mainMenuCanvas.interactable = false;
+        mainMenuCanvas.alpha = 0;
+        canvasBetweenRounds.interactable = false;
+        canvasBetweenRounds.alpha = 0;
+        gameCanvas.interactable = true;
+        gameCanvas.alpha = 1;
+        finishCanvas.interactable = false;
+        finishCanvas.alpha = 0;
+        deathCanvas.interactable = false;
+        deathCanvas.alpha = 0;
+        creditsCanvas.interactable = false;
+        creditsCanvas.alpha = 0;
+    }
+    public void DeathCanvas()
+    {
+        mainMenuCanvas.interactable = false;
+        mainMenuCanvas.alpha = 0;
+        canvasBetweenRounds.interactable = false;
+        canvasBetweenRounds.alpha = 0;
+        gameCanvas.interactable = false;
+        gameCanvas.alpha = 0;
+        finishCanvas.interactable = false;
+        finishCanvas.alpha = 0;
+        deathCanvas.interactable = true;
+        deathCanvas.alpha = 1;
+        creditsCanvas.interactable = false;
+        creditsCanvas.alpha = 0;
+    }
+    public void FinishCanvas()
+    {
+        mainMenuCanvas.interactable = false;
+        mainMenuCanvas.alpha = 0;
+        canvasBetweenRounds.interactable = false;
+        canvasBetweenRounds.alpha = 0;
+        gameCanvas.interactable = false;
+        gameCanvas.alpha = 0;
+        finishCanvas.interactable = true;
+        finishCanvas.alpha = 1;
+        deathCanvas.interactable = false;
+        deathCanvas.alpha = 0;
+        creditsCanvas.interactable = false;
+        creditsCanvas.alpha = 0;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
